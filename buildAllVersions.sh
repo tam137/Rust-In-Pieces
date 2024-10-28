31573cd79ab4fa8e57ea8e15bec633e5c131c18f
Jan Lange <tam137@live.de>
2024-10-28 17:31:18 +0100

    imporoved buildscript

--- buildAllVersions.sh
+++ buildAllVersions.sh
@@ -3,12 +3,21 @@
 cd $(dirname $0)
 WORKDIR=$PWD
 
+cargo test
+if [ $? -ne 0 ]; then
+  echo "Tests failed. Aborting build process."
+  exit 1
+fi
+
 cargo build --release
 cargo build --target x86_64-pc-windows-gnu --release
 cargo build --target=aarch64-unknown-linux-gnu --release
-cargo test
 
 mkdir -p buildVersions
+
 cp target/release/suprah buildVersions/suprah-x86
 cp target/aarch64-unknown-linux-gnu/release/suprah buildVersions/suprah-arm
 cp target/x86_64-pc-windows-gnu/release/suprah.exe buildVersions
+
+echo "Build and copy process completed successfully."
+
