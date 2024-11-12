6ff79d0ac450c6abfe83ce18500da084dc122987
Jan Lange <tam137@live.de>
2024-11-12 08:15:28 +0100

    fix div by zero in stats calculation

--- src/model.rs
+++ src/model.rs
@@ -490,7 +490,7 @@ impl Stats {
     pub fn calculate(&mut self) {
         self.cuts = 100 - (self.calculated_nodes as i32 * 100 / self.created_nodes as i32);
         self.capture_share = self.created_capture_node as i32 * 100 / self.created_nodes as i32;
-        self.nodes_per_ms = self.created_nodes as i32 / self.calc_time_ms as i32 + 1;
+        self.nodes_per_ms = self.created_nodes as i32 / (self.calc_time_ms as i32 + 1);
         self.zobrist_hit = self.zobrist_hit * 100 / self.eval_nodes;
     }
 
