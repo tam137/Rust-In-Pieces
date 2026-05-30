732ed0e95e8dfc3e72b4744c5e40b38d12578533
tam137 <tam137@live.de>
2026-05-30 09:20:38 +0200

    Update tuning.sh with optimal SPSA parameters

--- tuning/tuning.sh
+++ tuning/tuning.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 spsa_tuner.py --engine ../engines/suprah-0.11.4 --mm ../target/release/Matt-Magie --workers 2 --time 1 --inc 80 --mutate 5.0
+python3 spsa_tuner.py --engine ../engines/suprah-0.11.4/suprah --mm ../target/release/Matt-Magie --games 500 --workers 3 --time 1 --inc 80 --mutate 3.0
