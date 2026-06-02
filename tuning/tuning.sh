b1f5404c972215d14ac84e6ce54794b2841feda3
tam137 <tam137@live.de>
2026-06-02 22:20:47 +0200

    chore: update SPSA tuning target to suprah-0.13.3

--- tuning/tuning.sh
+++ tuning/tuning.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 spsa_tuner.py --engine ../engines/suprah-0.13.2 --mm ../target/release/Matt-Magie --games 750 --workers 3 --time 1 --inc 65 --mutate 3.0 --lr 15.0
+python3 spsa_tuner.py --engine ../engines/suprah-0.13.3 --mm ../target/release/Matt-Magie --games 750 --workers 3 --time 1 --inc 65 --mutate 3.0 --lr 15.0
