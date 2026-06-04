7b48b3f5b70ec5870d534f2ec2fa3cb651af1f1c
tam137 <tam137@live.de>
2026-06-04 18:54:31 +0200

    chore: update tuning script to target lmr_move_threshold with lr 18.0 and 300 games

--- tuning/tuning.sh
+++ tuning/tuning.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python3 spsa_tuner.py --engine ../engines/suprah-0.13.8 --mm ../target/release/Matt-Magie --games 750 --workers 3 --time 1 --inc 65 --mutate 3.0 --lr 4.0
+python3 spsa_tuner.py --engine ../engines/suprah-0.13.9 --mm ../target/release/Matt-Magie --games 300 --workers 3 --time 1 --inc 65 --mutate 3.0 --lr 18.0 --params lmr_move_threshold
+
