7e6f68b43f9a8321096f7e0116578d169e29abda
tam137 <tam137@live.de>
2026-06-05 11:12:03 +0200

    chore: configure SPSA tuning to target lmr_divisor on suprah-0.13.10

--- tuning/tuning.sh
+++ tuning/tuning.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-python3 spsa_tuner.py --engine ../engines/suprah-0.13.9 --mm ../target/release/Matt-Magie --games 300 --workers 3 --time 1 --inc 65 --mutate 3.0 --lr 18.0 --params lmr_move_threshold
+python3 spsa_tuner.py --engine ../engines/suprah-0.13.10 --mm ../target/release/Matt-Magie --games 300 --workers 3 --time 1 --inc 65 --mutate 3.0 --lr 5.0 --params lmr_divisor
+
 
