fec00b40f59f624e37459baad5be5079d354f8cc
tam137 <tam137@live.de>
2026-06-08 19:58:20 +0200

    Register new tuning parameters and configure SPSA for suprah-0.15.1

--- tuning/tuning.sh
+++ tuning/tuning.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 spsa_tuner.py --engine ../engines/suprah-0.13.11 --mm ../target/release/Matt-Magie --games 300 --workers 4 --time 2 --inc 100 --mutate 10.0 --lr 15.0 --params lmr_divisor
+python3 spsa_tuner.py --engine ../engines/suprah-0.15.1 --mm ../target/release/Matt-Magie --games 300 --workers 4 --time 2 --inc 100 --mutate 10.0 --lr 15.0 --params king_pawn_shield_kingside,king_pawn_shield_queenside,king_piece_shield_kingside,king_piece_shield_queenside,connected_passed_pawn_mg,connected_passed_pawn_eg,knight_outpost_true_mg,knight_outpost_true_eg,bishop_outpost_true_mg,bishop_outpost_true_eg,opposite_bishops_draw_scale,rook_behind_enemy_passed_pawn_mg,rook_behind_enemy_passed_pawn_eg
 
 
