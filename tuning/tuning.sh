09d441359b4518cede8f53da0f5f54381771266d
Jan Lange <tam137@live.de>
2026-08-03 10:50:13 +0200

    update engine in tuning.sh

--- tuning/tuning.sh
+++ tuning/tuning.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 spsa_tuner.py --group all --engine ../engines/suprah-0.18.1 --mm ../target/release/Matt-Magie --book ../books/Performance.bin --games 2500 --workers 4 --time 1 --inc 10 --mutate 10.0 --lr 5.0
+python3 spsa_tuner.py --group all --engine ../engines/suprah-0.23.12-nnue --mm ../target/release/Matt-Magie --book ../books/Performance.bin --games 2500 --workers 4 --time 1 --inc 10 --mutate 10.0 --lr 5.0
 
 
